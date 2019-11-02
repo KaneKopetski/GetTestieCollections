90f04ca06f789a193b01f406e4f995301da2256b
KaneKopetski <KaneKopetski@users.noreply.github.com>
2019-11-02 19:56:54 -0400

    Complete some tests

--- gettestie/src/test/java/rocks/zipcode/TestHashMap.java
+++ gettestie/src/test/java/rocks/zipcode/TestHashMap.java
@@ -1,31 +1,113 @@
 package rocks.zipcode;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class TestHashMap {
 
     @Test
     public void testPut() {
+        Map<Person, Address> addressMap = new HashMap();
+        Person person1 = new Person("Kane", 30);
+        Address address1 = new Address("Lynam Lookout Drive", "Newark", "19702");
+        addressMap.put(person1, address1);
+
+        Integer actual = addressMap.size();
+        Integer expected = 1;
+
+        Assert.assertEquals(expected, actual);
 
     }
 
     @Test
     public void testRemove() {
+        Map<Person, Address> addressMap = new HashMap();
+        Person person1 = new Person("Kane", 30);
+        Person person2 = new Person("John", 31);
+        Person person3 = new Person("Bob", 21);
+
+        Address address1 = new Address("Lynam Lookout Drive", "Newark", "19702");
+        Address address2 = new Address("Oak Street", "Sanford", "04073");
+        Address address3 = new Address("Graham Drive", "Wilmington", "19808");
+
+        addressMap.put(person1, address1);
+        addressMap.put(person2, address2);
+        addressMap.put(person3, address3);
+
+        addressMap.remove(person1, address1);
+
+        Integer actual = addressMap.size();
+        Integer expected = 2;
+
+        Assert.assertEquals(expected, actual);
 
     }
 
     @Test
     public void testSize() {
+        Map<Person, Address> addressMap = new HashMap();
+        Person person1 = new Person("Kane", 30);
+        Person person2 = new Person("John", 31);
+        Person person3 = new Person("Bob", 21);
+
+        Address address1 = new Address("Lynam Lookout Drive", "Newark", "19702");
+        Address address2 = new Address("Oak Street", "Sanford", "04073");
+        Address address3 = new Address("Graham Drive", "Wilmington", "19808");
+
+        addressMap.put(person1, address1);
+        addressMap.put(person2, address2);
+        addressMap.put(person3, address3);
+
+        Integer actual = addressMap.size();
+        Integer expected = 3;
+
+        Assert.assertEquals(expected, actual);
 
     }
 
     @Test
     public void testContainsKey() {
+        Map<Person, Address> addressMap = new HashMap();
+        Person person1 = new Person("Kane", 30);
+        Person person2 = new Person("John", 31);
+        Person person3 = new Person("Bob", 21);
+
+        Address address1 = new Address("Lynam Lookout Drive", "Newark", "19702");
+        Address address2 = new Address("Oak Street", "Sanford", "04073");
+        Address address3 = new Address("Graham Drive", "Wilmington", "19808");
+
+        addressMap.put(person1, address1);
+        addressMap.put(person2, address2);
+        addressMap.put(person3, address3);
+
+        Assert.assertTrue(addressMap.containsKey(person1));
 
     }
 
     @Test
     public void testClear() {
+        Map<Person, Address> addressMap = new HashMap();
+        Person person1 = new Person("Kane", 30);
+        Person person2 = new Person("John", 31);
+        Person person3 = new Person("Bob", 21);
+
+        Address address1 = new Address("Lynam Lookout Drive", "Newark", "19702");
+        Address address2 = new Address("Oak Street", "Sanford", "04073");
+        Address address3 = new Address("Graham Drive", "Wilmington", "19808");
+
+        addressMap.put(person1, address1);
+        addressMap.put(person2, address2);
+        addressMap.put(person3, address3);
+
+        addressMap.clear();
+
+        Integer actual = addressMap.size();
+        Integer expected = 0;
+
+        Assert.assertEquals(expected, actual);
 
     }
 }
--- gettestie/src/test/java/rocks/zipcode/TestHashSet.java
+++ gettestie/src/test/java/rocks/zipcode/TestHashSet.java
@@ -1,31 +1,98 @@
 package rocks.zipcode;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class TestHashSet {
 
     @Test
     public void testAdd() {
+        Set<Person> personSet = new HashSet<>();
+        Person person1 = new Person("Kane", 30);
+        Person person2 = new Person("John", 31);
+        Person person3 = new Person("Bob", 21);
+
+        personSet.add(person1);
+        personSet.add(person2);
+        personSet.add(person3);
+
+        Integer actual = personSet.size();
+        Integer expected = 3;
+
+        Assert.assertEquals(expected, actual);
 
     }
 
     @Test
     public void testRemove() {
+        Set<Person> personSet = new HashSet<>();
+        Person person1 = new Person("Kane", 30);
+        Person person2 = new Person("John", 31);
+        Person person3 = new Person("Bob", 21);
+
+        personSet.add(person1);
+        personSet.add(person2);
+        personSet.add(person3);
+
+        personSet.remove(person2);
+
+        Integer actual = personSet.size();
+        Integer expected = 2;
+
+        Assert.assertEquals(expected, actual);
 
     }
 
     @Test
     public void testSize() {
+        Set<Person> personSet = new HashSet<>();
+        Person person1 = new Person("Kane", 30);
+        Person person2 = new Person("John", 31);
+        Person person3 = new Person("Bob", 21);
+
+        personSet.add(person1);
+        personSet.add(person2);
+        personSet.add(person3);
+
+        Integer actual = personSet.size();
+        Integer expected = 3;
+
+        Assert.assertEquals(expected, actual);
 
     }
 
     @Test
     public void testContains() {
+        Set<Person> personSet = new HashSet<>();
+        Person person1 = new Person("Kane", 30);
+        Person person2 = new Person("John", 31);
+        Person person3 = new Person("Bob", 21);
+
+        personSet.add(person1);
+        personSet.add(person2);
+        personSet.add(person3);
+
+        Assert.assertTrue(personSet.contains(person3));
 
     }
 
     @Test
     public void testToString() {
+        Set<Person> personSet = new HashSet<>();
+        Person person1 = new Person("Kane", 30);
+        Person person2 = new Person("John", 31);
+        Person person3 = new Person("Bob", 21);
 
+        personSet.add(person1);
+        personSet.add(person2);
+        personSet.add(person3);
+
+        String expected = "[Name: Bob\nYear of birth: 21\n, Name: John\nYear of birth: 31\n, Name: Kane\nYear of birth: 30\n]";
+        String actual = personSet.toString();
+
+        Assert.assertEquals(expected, actual);
     }
 }
